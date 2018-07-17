666276e935b3713c2c62d99dd736455d00c6e0bf
Richard Towers <richard.towers@digital.cabinet-office.gov.uk>
2018-07-17 17:02:00 +0100

    Enable HTTPS communication with the backend

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,19 @@
+const https = require('https')
+const selfsigned = require('selfsigned')
+const attrs = [{ name: 'commonName', value: 'Backend test certificate' }]
+const pems = selfsigned.generate(attrs, { days: 365 })
 const express = require('express')
+const app = express()
 
-const app = express();
+const httpsServer = https.createServer({ key: pems.private, cert: pems.cert }, app)
+
+const startTime = new Date().toISOString()
 
 app.all('*', (req, res) => {
-	res.send('Hello from the backend. The time is ' + new Date().toISOString())
+  res.send(`Hello from the backend
+I was started at ${startTime}.
+The time now is ${new Date().toISOString()}
+`)
 })
 
-app.listen(process.env.PORT || 3001)
+httpsServer.listen(process.env.PORT || 3001)
