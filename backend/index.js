9389a777795992bcc337ce9f7bbd2e9652bb1cf1
Richard Towers <richard.towers@digital.cabinet-office.gov.uk>
2018-07-18 12:02:38 +0100

    Add the ability to kill the backend for testing

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,22 @@
 const express = require('express')
 
 const app = express();
+const startTime = new Date().toISOString()
+
+let dead = false
+
+app.post('/die', (req, res) => (dead = true, res.send('💀')))
 
 app.all('*', (req, res) => {
-	res.send('Hello from the backend. The time is ' + new Date().toISOString())
+  if (dead) {
+    throw '💀 ' + req.headers['user-agent']
+  }
+  else {
+    res.send(`Hello from the backend
+I was started at ${startTime}.
+The time now is ${new Date().toISOString()}
+`)
+  }
 })
 
 app.listen(process.env.PORT || 3001)
