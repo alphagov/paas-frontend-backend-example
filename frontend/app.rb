90fd1cc252177f39fe6998521f2b51830faab410
Richard Towers <richard.towers@digital.cabinet-office.gov.uk>
2018-10-29 13:54:46 +0000

    Make the frontend show images from the backend

--- frontend/app.rb
+++ frontend/app.rb
@@ -1,10 +1,28 @@
 require 'sinatra/base'
+require 'open-uri'
+
+def get_art_from_backend()
+  backend_url = ENV.fetch('BACKEND_URL')
+  result = nil
+  open(backend_url) do |f|
+    result = JSON.parse(f.read)
+    logger.info(result)
+  end
+  result
+end
 
 class Frontend < Sinatra::Base
+  enable :logging
   set :public_folder, File.dirname(__FILE__) + '/static'
 
   get "/" do
-    erb :index, layout: :layout
+    begin
+      art = get_art_from_backend
+      erb :index, layout: :layout, locals: art
+    rescue StandardError => err
+      logger.error "Unable to reach backend: #{err.inspect}"
+      erb :error, layout: :layout
+    end
   end
 
   run! if __FILE__ == $0
